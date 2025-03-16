6f1e87f271b9f32ef7b3c41d2681175ac7326960
Ananthadatta02 <sarvadeananthadatta143@gmail.com>
2025-03-16 12:03:34 +0530

    Adding files

---
+++ SubStrings_of_String.java
@@ -0,0 +1,17 @@
+package subString;
+
+public class SubStrings_of_String 
+{
+	public static void main(String[] args) 
+	{
+        String str = "abc";
+        System.out.println("All substrings of '" + str + "':");
+        for (int i = 0; i < str.length(); i++) 
+        {  
+            for (int j = i + 1; j <= str.length(); j++) 
+            {  
+                System.out.println(str.substring(i, j));  
+            }  
+        }  
+    }
+}
